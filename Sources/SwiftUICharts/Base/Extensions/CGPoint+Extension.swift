1d1ba895eacc6594d45ab2f4b93dd156d08229e7
Dan Wood <danwood@users.noreply.github.com>
2020-08-18 17:14:28 -0700

    move yrange calculation out

--- Sources/SwiftUICharts/Base/Extensions/CGPoint+Extension.swift
+++ Sources/SwiftUICharts/Base/Extensions/CGPoint+Extension.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension CGPoint {
-	static func getStep(frame: CGRect, lineWidth: CGFloat, data: [Double], limits: ChartLimits ) -> CGPoint {
+	static func getStep(frame: CGRect, lineWidth: CGFloat, data: [Double], yRange: Range<Double> ) -> CGPoint {
 
 		// Linewidth is subtracted from frame height so thick lines don't get clipped. Drawing needs to also take lineWidth into account.
 
@@ -14,114 +14,17 @@ extension CGPoint {
 		// stepHeight
 		var stepHeight: CGFloat = 0.0
 
-		var minimumValue: Double = 0.0
-		var maximumValue: Double = 0.0
-
-		if case .explicit(let range) = limits.yLimit {
-			minimumValue = range.lowerBound
-			maximumValue = range.upperBound
-		}
-		else {
-			maximumValue = data.max() ?? 0.0	// get the data's max, but then we'll probably adjust below
-
-			if limits.symmetrical {
-				// If we want symmetrical above and below Y axis, we have to take absolute value of negative values into account
-				let minMagnitude = abs(data.min() ?? 0.0)
-				maximumValue = max(maximumValue, minMagnitude)
-			}
-
-			// Now adjust maximum from data to round up to a nice value
-
-			switch(limits.yLimit) {
-				case .powerOfTen:
-					// bump up to 10, 100, 1000 etc.
-					let numPlaces = ceil(log10(maximumValue))	// round up the log10
-					maximumValue = __exp10(numPlaces)
-					print("powerOfTen \(maximumValue)")
-				case .halfPowerOfTen:
-					// bump up to 5, 10, 50, 100, 500, 1000
-					let theLog10 = log10(maximumValue)
-					let fiveLog10 = log10(5.0)
-					let theFloor = floor(theLog10)
-					let fractionalLog10 = theLog10 - theFloor
-					if fractionalLog10 >= fiveLog10 {
-						maximumValue = __exp10(theFloor + 1)	// max is 10, 100, 1000 etc.
-						print("halfPowerOfTen startingWithTen \(maximumValue)")
-					}
-					else {
-						maximumValue = __exp10(theFloor + fiveLog10)	// max 5, 50, 500, 5000 etc.
-						print("halfPowerOfTen startingWithFive \(maximumValue)")
-					}
-				case .firstSignificant:
-
-					print("firstSignificant starting max = \(maximumValue)")
-																	// 98  ; 1000 ; 5678
-					var numPlaces = 1 + floor(log10(maximumValue))			// 2   ; 4    ; 4
-					print("numPlaces = \(numPlaces)")
-					let normalized = maximumValue / __exp10(numPlaces - 1) 	// 9.8 ; 1.000; 5.678
-					print("normalized = \(normalized)")
-					let normalizedRoundedUp = ceil(normalized)		// 10.0; 1.0; 6.0
-					print("normalizedRoundedUp = \(normalizedRoundedUp)")
-					// Problem: 9.x gets rounded up to 10, which we won't want, so reduce multiplier by factor of 10
-					if normalizedRoundedUp > 9 { numPlaces -= 1 }	// 10  ; 1000 ; 1000
-					print("numPlaces = \(numPlaces)")
-					let multiplier = __exp10(numPlaces)				// 100 ; 1000 ; 1000
-					print("multiplier = \(multiplier)")
-					maximumValue = normalizedRoundedUp * multiplier			// 100 ; 1000 ; 6000
-					print("firstSignificant max = \(maximumValue)")
-
-				case .secondSignificant:
-
-					print("secondSignificant starting max = \(maximumValue)")
-																	// 98  ; 1000 ; 5678
-					var numPlaces = 1 + floor(log10(maximumValue))			// 2   ; 4    ; 4
-					print("numPlaces = \(numPlaces)")
-					let normalized = maximumValue / __exp10(numPlaces - 2) 	// 98 ; 10.00; 56.78
-					print("normalized = \(normalized)")
-					let normalizedRoundedUp = ceil(normalized)		// 98 ; 10   ; 57
-					print("normalizedRoundedUp = \(normalizedRoundedUp)")
-					// Problem: 9.x gets rounded up to 10, which we won't want, so reduce multiplier by factor of 10
-					if normalizedRoundedUp > 9 { numPlaces -= 1 }
-					print("numPlaces = \(numPlaces)")
-					let multiplier = __exp10(numPlaces-1)			// 1   ; 100  ; 100
-					print("multiplier = \(multiplier)")
-					maximumValue = normalizedRoundedUp * multiplier			// 980 ; 1000 ; 5700
-					print("secondSignificant max = \(maximumValue)")
-
-					break;
-
-				case .fromData:
-					break;
-
-				case .explicit(_):
-					break;
-					// already handled
-			}
-		}
-
-		if limits.symmetrical {
-			minimumValue = -maximumValue	// same below the axis as above
-		}
-		else {
-			// Not symmetrical - now we get a chance to look at any minimum specified
-
-			minimumValue = data.min() ?? 0.0
 
 
-			// TODO
-
-			
-		}
-
-		print("RANGE: \(minimumValue) - \(maximumValue)")
+		print("RANGE: \(yRange.lowerBound) - \(yRange.upperBound)")
 
 
-		if minimumValue != maximumValue {
-			if minimumValue <= 0 {
-				stepHeight = (frame.size.height - lineWidth) / CGFloat(maximumValue + abs(minimumValue))	// negative number means include span between neg and pos
+		if yRange.lowerBound != yRange.upperBound {
+			if yRange.lowerBound <= 0 {
+				stepHeight = (frame.size.height - lineWidth) / CGFloat(yRange.upperBound + abs(yRange.lowerBound))	// negative number means include span between neg and pos
 				print("negative minimum. stepHeight = \(stepHeight)")
 			} else {
-				stepHeight = (frame.size.height - lineWidth) / CGFloat(maximumValue - minimumValue)
+				stepHeight = (frame.size.height - lineWidth) / CGFloat(yRange.upperBound - yRange.lowerBound)
 				print("positive minimum. stepHeight = \(stepHeight)")
 			}
 			return CGPoint(x: stepWidth, y: stepHeight)
--- Sources/SwiftUICharts/Charts/LineChart/Line.swift
+++ Sources/SwiftUICharts/Charts/LineChart/Line.swift
@@ -10,8 +10,115 @@ public struct Line: View {
     @State var showIndicator: Bool = false
     @State var touchLocation: CGPoint = .zero
     @State private var showFull: Bool = false
-    var step: CGPoint {
-		return CGPoint.getStep(frame: frame, lineWidth: style.lineWidth, data: chartData.data, limits:style.limits)
+
+	// Calculate yRange we're using based on data and style preference
+	var yRange: Range<Double> {
+
+		var minimumValue: Double = 0.0
+		var maximumValue: Double = 0.0
+
+		if case .explicit(let range) = style.limits.yLimit {
+			minimumValue = range.lowerBound
+			maximumValue = range.upperBound
+		}
+		else {
+			maximumValue = chartData.data.max() ?? 0.0	// get the data's max, but then we'll probably adjust below
+
+			if style.limits.symmetrical {
+				// If we want symmetrical above and below Y axis, we have to take absolute value of negative values into account
+				let minMagnitude = abs(chartData.data.min() ?? 0.0)
+				maximumValue = max(maximumValue, minMagnitude)
+			}
+
+			// Now adjust maximum from data to round up to a nice value
+
+			switch(style.limits.yLimit) {
+				case .powerOfTen:
+					// bump up to 10, 100, 1000 etc.
+					let numPlaces = ceil(log10(maximumValue))	// round up the log10
+					maximumValue = __exp10(numPlaces)
+					print("powerOfTen \(maximumValue)")
+				case .halfPowerOfTen:
+					// bump up to 5, 10, 50, 100, 500, 1000
+					let theLog10 = log10(maximumValue)
+					let fiveLog10 = log10(5.0)
+					let theFloor = floor(theLog10)
+					let fractionalLog10 = theLog10 - theFloor
+					if fractionalLog10 >= fiveLog10 {
+						maximumValue = __exp10(theFloor + 1)	// max is 10, 100, 1000 etc.
+						print("halfPowerOfTen startingWithTen \(maximumValue)")
+					}
+					else {
+						maximumValue = __exp10(theFloor + fiveLog10)	// max 5, 50, 500, 5000 etc.
+						print("halfPowerOfTen startingWithFive \(maximumValue)")
+					}
+				case .firstSignificant:
+
+					print("firstSignificant starting max = \(maximumValue)")
+					// 98  ; 1000 ; 5678
+					var numPlaces = 1 + floor(log10(maximumValue))			// 2   ; 4    ; 4
+					print("numPlaces = \(numPlaces)")
+					let normalized = maximumValue / __exp10(numPlaces - 1) 	// 9.8 ; 1.000; 5.678
+					print("normalized = \(normalized)")
+					let normalizedRoundedUp = ceil(normalized)		// 10.0; 1.0; 6.0
+					print("normalizedRoundedUp = \(normalizedRoundedUp)")
+					// Problem: 9.x gets rounded up to 10, which we won't want, so reduce multiplier by factor of 10
+					if normalizedRoundedUp > 9 { numPlaces -= 1 }	// 10  ; 1000 ; 1000
+					print("numPlaces = \(numPlaces)")
+					let multiplier = __exp10(numPlaces)				// 100 ; 1000 ; 1000
+					print("multiplier = \(multiplier)")
+					maximumValue = normalizedRoundedUp * multiplier			// 100 ; 1000 ; 6000
+					print("firstSignificant max = \(maximumValue)")
+
+				case .secondSignificant:
+
+					print("secondSignificant starting max = \(maximumValue)")
+					// 98  ; 1000 ; 5678
+					var numPlaces = 1 + floor(log10(maximumValue))			// 2   ; 4    ; 4
+					print("numPlaces = \(numPlaces)")
+					let normalized = maximumValue / __exp10(numPlaces - 2) 	// 98 ; 10.00; 56.78
+					print("normalized = \(normalized)")
+					let normalizedRoundedUp = ceil(normalized)		// 98 ; 10   ; 57
+					print("normalizedRoundedUp = \(normalizedRoundedUp)")
+					// Problem: 9.x gets rounded up to 10, which we won't want, so reduce multiplier by factor of 10
+					if normalizedRoundedUp > 9 { numPlaces -= 1 }
+					print("numPlaces = \(numPlaces)")
+					let multiplier = __exp10(numPlaces-1)			// 1   ; 100  ; 100
+					print("multiplier = \(multiplier)")
+					maximumValue = normalizedRoundedUp * multiplier			// 980 ; 1000 ; 5700
+					print("secondSignificant max = \(maximumValue)")
+
+					break;
+
+				case .fromData:
+					break;
+
+				case .explicit(_):
+					break;
+				// already handled
+			}
+		}
+
+		if style.limits.symmetrical {
+			minimumValue = -maximumValue	// same below the axis as above
+		}
+		else {
+			// Not symmetrical - now we get a chance to look at any minimum specified
+
+			minimumValue = chartData.data.min() ?? 0.0
+
+
+			// TODO
+
+
+		}
+
+		print("RANGE: \(minimumValue) - \(maximumValue)")
+		return minimumValue ..< maximumValue
+	}
+
+	var step: CGPoint {
+		return CGPoint.getStep(frame: frame, lineWidth: style.lineWidth, data: chartData.data, yRange:yRange)
     }
 
     var path: Path {
